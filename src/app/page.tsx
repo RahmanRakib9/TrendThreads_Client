5ea67466bcad0ce9d6af9f3522753a885e167984
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-05-21 12:04:57 +0600

    feat:impplement revalidation on flashSales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,11 @@ import FlashSale from '@/components/FlashSale/FlashSale';
 import React from 'react';
 
 const Home = async () => {
-  const res = await fetch("http://localhost:3004/api/v1/flash-sales")
+  const res = await fetch("http://localhost:3004/api/v1/flash-sales", {
+    next: {
+      revalidate: 30
+    }
+  })
   const flashSales = await res.json();
   return (
     <div>
