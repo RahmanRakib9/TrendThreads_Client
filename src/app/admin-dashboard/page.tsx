70ca89a7cd1d4f9e126582ce8b32429ab07df24c
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-05-22 16:55:21 +0600

    feat:add admin dashboard

--- src/app/admin-dashboard/page.tsx
+++ src/app/admin-dashboard/page.tsx
@@ -1,9 +1,15 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AdminDashboard = () => {
     return (
-        <div>
-            this is admin dashboard
+        <div className='my-10'>
+            <h1 className='text-2xl text-pretty text-center'><b>Welcome to Admin Dashboard!</b></h1>
+            <div className='flex flex-col space-y-8'>
+                <Link href="/flash-sales">Flash Sales</Link>
+                <Link href="/category">Categories</Link>
+                <Link href="/products">Products</Link>
+            </div>
         </div>
     );
 };
